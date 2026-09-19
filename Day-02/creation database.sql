e3c4d32da56dacbb5943cbefe8962b0869b942c5
Abilash02-10 <abilashreddyalugonda@gmail.com>
2026-09-19 09:14:01 -0700

    creation of databse

---
+++ Day-02/creation database.sql
@@ -0,0 +1,16 @@
+-- create database
+create database pfs63;
+-- check database
+show databases;
+-- delete database
+drop database pfs63;
+
+-- select database
+use pfs63;
+
+-- how to create table
+create table  abilash(
+columname datatype constrain
+);
+
+
